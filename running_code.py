027fd7cf834ab4d9fbebd4150ae479144c4ff016
ahmadreza <nazariahmadreza@gmail.com>
2024-11-26 11:50:29 +0100

    adding libraries temporarily for the project

---
+++ running_code.py
@@ -0,0 +1,27 @@
+from impl import Handler, MetadataUploadHandler, ProcessDataUploadHandler
+import pandas as pd
+
+hand = Handler()
+
+print(hand.getDbPathOrUrl())
+
+#path = 'data/process.json'
+#hand.setDbPathOrUrl(path)
+#file = hand.getDbPathOrUrl()
+#print(file)
+
+#process = ProcessDataUploadHandler()
+
+#df = process.pushDataToDb(file)
+#print(df)
+
+#df.to_csv('test.csv')(
+
+path = 'data/meta.csv'
+hand.setDbPathOrUrl(path)
+hand.getDbPathOrUrl()
+
+meta = MetadataUploadHandler()
+
+test = meta.pushDataToDb(hand.getDbPathOrUrl())
+print(test)
