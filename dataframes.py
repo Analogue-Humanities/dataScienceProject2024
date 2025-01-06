bcbebc71db4c86df7f7bea958bfa8e67e53efb81
ahmadreza <nazariahmadreza@gmail.com>
2025-01-06 23:13:49 +0100

    removing extra codes in dataframes file

--- dataframes.py
+++ dataframes.py
@@ -5,27 +5,6 @@ from sqlite3 import connect
 from pandas import read_sql, DataFrame, concat
 from sparql_dataframe import get
 
-# Reading from the sql database, as a sample just a simple query
-#with connect('Data.db') as con:
-#    query = 'SELECT * FROM Acquisition'
-#    df_sql = read_sql(query, con)
-#    print(df_sql)
-'''
-# Reading from the blazegraph, the Graph database, using a simple query as a sample
-endpoint = "http://127.0.0.1:9999/blazegraph/sparql"
-query2 = """
-PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-PREFIX schema: <https://schema.org/>
-SELECT ?title
-WHERE {
-    ?s schema:identifier "2".
-  	?s schema:name ?title.
-}
-"""
-
-df_sparql = get(endpoint, query2, True)
-print(df_sparql)
-'''
 # I implement the remaining classes here in order to avoid several uploads to th egraph database. when completed I will
 # merge them all.
 
